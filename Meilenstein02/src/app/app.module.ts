241fdf75d7be45b1024a5a089b69ade54c7b4b8e
Luca Schöneberg <luca-schoeneberg@outlook.com>
2023-10-19 11:27:56 +0200

    add modal and placeholder

--- Meilenstein02/src/app/app.module.ts
+++ Meilenstein02/src/app/app.module.ts
@@ -8,17 +8,22 @@ import {AboutComponent} from './features/about/page/about.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {NgOptimizedImage} from "@angular/common";
 import {NavbarComponent} from "./core/components/navbar/navbar.component";
+import {PlaceholderComponent} from './features/placeholder/page/placeholder.component';
+import { ModalComponent } from './core/components/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    NavbarComponent
+    NavbarComponent,
+    PlaceholderComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: 'about', component: AboutComponent},
+      {path: 'placeholder', component: PlaceholderComponent}
     ]),
     NgbModule,
     NgOptimizedImage,

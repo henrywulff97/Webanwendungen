557656118fa396343008c08afffc88c9cf75500f
Henry Wulff <72397398+henrywulff97@users.noreply.github.com>
2023-10-19 11:57:39 +0200

    yourrecords weiter gemacht

--- Meilenstein02/src/app/features/yourrecords/yourrecords.component.ts
+++ Meilenstein02/src/app/features/yourrecords/yourrecords.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./yourrecords.component.scss']
 })
 export class YourrecordsComponent {
-
+  records = [
+    {
+      albumName: "Peripheral Vision",
+      artist: "Turnover",
+      version: "First Print",
+      releaseDate: "19.07.1997",
+      recordLabel: "Fueled by Ramen"
+    },
+    {
+      albumName: "Why Would I Watch",
+      artist: "Hot Mulligan",
+      version: "First Print",
+      releaseDate: "19.07.1997",
+      recordLabel: "Fueled by Ramen"
+    },
+    {
+      albumName: "Vessel",
+      artist: "Twenty One Pilots",
+      version: "First Print",
+      releaseDate: "19.07.1997",
+      recordLabel: "Fueled by Ramen"
+    },
+    {
+      albumName: "Blue In The Dark",
+      artist: "Bearings",
+      version: "First Print",
+      releaseDate: "19.07.1997",
+      recordLabel: "Fueled by Ramen"
+    },
+    {
+      albumName: "Tickets To My Downfall",
+      artist: "Machine Gun Kelly",
+      version: "First Print",
+      releaseDate: "19.07.1997",
+      recordLabel: "Fueled by Ramen"
+    }
+  ]
 }

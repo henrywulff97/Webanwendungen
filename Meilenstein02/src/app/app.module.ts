46576b9c06f58f7243ef489e6df32abbfc1a935b
Luca Schöneberg <luca-schoeneberg@outlook.com>
2023-10-19 12:24:49 +0200

    add localstorage

--- Meilenstein02/src/app/app.module.ts
+++ Meilenstein02/src/app/app.module.ts
@@ -9,9 +9,10 @@ import {NgOptimizedImage} from "@angular/common";
 import {NavbarComponent} from "./core/components/navbar/navbar.component";
 import {PlaceholderComponent} from './features/placeholder/page/placeholder.component';
 import {FooterComponent} from './core/components/footer/footer.component';
-import {NgbCarousel} from "@ng-bootstrap/ng-bootstrap";
+import {NgbCarousel, NgbSlide} from "@ng-bootstrap/ng-bootstrap";
 import { HomeComponent } from './features/home/home.component';
 import { YourrecordsComponent } from './features/yourrecords/yourrecords.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -33,7 +34,9 @@ import { YourrecordsComponent } from './features/yourrecords/yourrecords.compone
       {path: '', component: HomeComponent}
     ]),
     NgOptimizedImage,
-    NgbCarousel
+    NgbCarousel,
+    NgbSlide,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

a07eb2f358a835a0826ae3eba1a3edd7f14aaf11
Henry Wulff <72397398+henrywulff97@users.noreply.github.com>
2023-11-02 09:34:19 +0100

    added correct tabs

--- Meilensten 3/Recordz/src/app/app-routing.module.ts
+++ Meilensten 3/Recordz/src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+  },
+  {
+    path: 'data',
+    loadChildren: () => import('./data/data.module').then( m => m.DataPageModule)
   }
 ];
 @NgModule({

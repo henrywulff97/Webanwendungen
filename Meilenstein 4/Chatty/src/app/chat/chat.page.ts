22f72a5893a924d8ae18830d014eaa153f5a5238
Henry Wulff <henrywulff97@gmail.com>
2023-11-09 15:52:52 +0100

    vorlage projekt meilenstein 5 importiert

--- Meilenstein 4/Chatty/src/app/chat/chat.page.ts
+++ Meilenstein 4/Chatty/src/app/chat/chat.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { inject } from '@angular/core';
-import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, doc, getDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -13,9 +13,10 @@ export class ChatPage implements OnInit {
   firestore: Firestore = inject(Firestore);
 
   constructor() {
-    const itemCollection = collection(this.firestore, 'items');
+    const itemCollection = collection(this.firestore, 'room_0');
     this.item$ = collectionData(itemCollection);
     console.log('Data from Firebase:', this.item$);
+
   }
 
   ngOnInit() {

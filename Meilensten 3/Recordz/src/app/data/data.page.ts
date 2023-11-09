9507ded72aab2457f3d741e899b549c023ef35af
Luca Schöneberg <luca-schoeneberg@outlook.com>
2023-11-09 12:35:04 +0100

    add list to data and counter info on tabicon data

--- Meilensten 3/Recordz/src/app/data/data.page.ts
+++ Meilensten 3/Recordz/src/app/data/data.page.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+interface itemData {
+  albumName: string;
+  artist: string;
+  version: string;
+  releaseDate: string;
+  recordLabel: string;
+}
 
 @Component({
   selector: 'app-data',
@@ -6,8 +14,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data.page.scss'],
 })
 export class DataPage implements OnInit {
+  items: itemData[] = [
+    {
+      albumName: 'Album 1',
+      artist: 'Artist 1',
+      version: '1.0',
+      releaseDate: '2023-01-01',
+      recordLabel: 'Label 1'
+    },
+    {
+      albumName: 'Album 2',
+      artist: 'Artist 2',
+      version: '1.0',
+      releaseDate: '2023-01-02',
+      recordLabel: 'Label 2'
+    }
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }

ced6cbb45d7961935f166f11476c53aa39eea8fb
Henry Wulff <henrywulff97@gmail.com>
2023-11-12 14:15:49 +0100

    meilenstein 4 zweiter versuch

---
+++ Meilenstein 4/Chatty2/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Chatty2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;

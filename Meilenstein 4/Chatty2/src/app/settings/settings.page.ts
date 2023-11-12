a021ebc91def4ad67ebaa3f119285d5bf3c77914
Henry Wulff <henrywulff97@gmail.com>
2023-11-12 15:06:59 +0100

    meilenstein 4 settings ohne unique abgleich

--- Meilenstein 4/Chatty2/src/app/settings/settings.page.ts
+++ Meilenstein 4/Chatty2/src/app/settings/settings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
+  username: string | undefined;
+  constructor(private storage: Storage) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    await this.storage.create();
+    this.storage.get('savedContent').then((value) => {
+      this.username = value || '';
+    });
   }
 
+  async save() {
+    await this.storage.create();
+
+    this.storage.set('username', this.username);
+  }
 }
